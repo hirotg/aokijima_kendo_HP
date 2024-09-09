de2991ae8f12904080856d52c66ca8b06fb3e8d2
hirotg <hiroakzutg@gmail.com>
2024-09-09 18:16:17 +0900

    テーブルボーダー

--- public_html/event/event.php
+++ public_html/event/event.php
@@ -25,9 +25,9 @@
     }else{
 
         print "ここに 大会の要項、選手、監督の情報を載せる";
-        print '<table>';
+        print '<table border="1">';
 
-        print '<tr><td>2024/9/14(土)</td><td>保護者総会</td><td><a href="20240914_hogosya_soukai.pdf">"保護者総会資料"></a></td><td></td></tr>';
+        print '<tr><td>2024/9/14(土)</td><td>保護者総会</td><td><a href="20240914_hogosya_soukai.pdf">保護者総会資料</a></td><td></td></tr>';
         print '<tr><td>2024/9/22(土)</td><td>長野市柔剣道大会</td><td></td><td>小学生団体</td></tr>';
 
         print '</table>';
